a89aa9553422b3111b56e97440ec1dbb24d34b03
anderdc <anderdc_19@yahoo.com>
2025-07-04 01:29:27 +0000

    more nn work

--- extraction/LT.py
+++ extraction/LT.py
@@ -100,12 +100,11 @@ class LTPipeline:
         # assign to object for easily grabbing values again
         self.X_train, self.X_test, self.y_train, self.y_test = X_train, X_test, y_train, y_test
 
-        if self.verbose:
-            print('*'*20, 'Train Test Split results', '*'*20) 
-            print(f"X_train shape: {X_train.shape}")
-            print(f"X_test shape: {X_test.shape}")
-            print(f"y_train shape: {y_train.shape}")
-            print(f"y_test shape: {y_test.shape}")
+        print('*'*20, 'Train Test Split results', '*'*20) 
+        print(f"X_train shape: {X_train.shape}")
+        print(f"X_test shape: {X_test.shape}")
+        print(f"y_train shape: {y_train.shape}")
+        print(f"y_test shape: {y_test.shape}")
         
         return X_train, X_test, y_train, y_test
 
